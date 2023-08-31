88a187d8b80fb019c2fd62a6e72828e33d52563b
Sandra Liu <sara@frad.in>
2023-08-31 13:46:51 +0200

    Http request to fetch data upon launching.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { setVerbs } from './store/verbs/verbs.actions';
+import { Store, select } from '@ngrx/store';
+import { Observable, map } from 'rxjs';
+import { VerbState } from './store/verbs/verbs.reducer';
+import { HttpClient } from '@angular/common/http';
+import { VerbsList } from './interface/verbs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  /** RETURN DATA */
   title = 'language-app';
+  private URL = 'http://localhost:8080/verbs/allverbs.php';
+
+  /** CONSTRUCTOR */
+  constructor(private httpClient: HttpClient, private store: Store<{ verbsStore: VerbState }>) {}
+
+  /** METHODS */
+  ngOnInit() {
+    // at the start of an app we do http request to load verbs in the store
+    this.httpClient.get<VerbsList>(this.URL).subscribe((verbs) => {
+      this.store.dispatch(setVerbs({ allVerbs: verbs}))
+    })
+  }
 }
