55c1f2964b1af4ca818c9f276ff0eea10f1f1dbc
Sandra Liu <sara@frad.in>
2023-08-31 13:41:38 +0200

    Project setup.

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'black-olive': '#3E3E3E',
+        'foam': '#D2FEF5',
+        'pastel-pink': '#FED2DB',
+        'light-aquamarine': '#A1FCEA',
+        'flamingo': '#FDA0B3',
+        'rangoon-green': '#1D1B1B',
+        'soapstone': '#FFFAFA',
+        /** PURPLES - PINK for MATERIAL */
+        'watermelon-pink': '#FC6E8B',
+        'purplish-pink': '#D959AC',
+        'pearl': '#DEE1DF',
+        'ghost-white': '#F9F9F9',
+        'purple-heart': '#673ab7'
+      },
+    },
+  },
+  plugins: [],
+}
+
