11a007a71e884776156439e527f4effeaffb7b66
Sandra Liu <sara@frad.in>
2023-08-31 13:44:45 +0200

    Create routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VerbDisplayComponent } from './verb-display/verb-display.component';
+import { VerbPracticeTemplateComponent } from './verb-practice-template/verb-practice-template.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'display', component: VerbDisplayComponent },
+  { path: 'practice/:verb', component: VerbPracticeTemplateComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
