7d936835e08c9d7c70b1efef19aa9cd1722772a7
Sandra Liu <sandra@fedora-2.home>
2024-06-16 18:15:52 +0200

    Added chart to dashboard

--- src/app/Components/chart/chart.component.ts
+++ src/app/Components/chart/chart.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.scss']
 })
-export class ChartComponent {
 
+export class ChartComponent {
+  @Input() chartParentWidth:number;
+  @Input() chartParentHeight:number;
+
+
+  data = [
+    {
+      "name": "1 Jul",
+      "value": 23
+    },
+    {
+      "name": "2 Jul",
+      "value": 5
+    },
+    {
+      "name": "3 Jul",
+      "value": 43
+    },
+    {
+      "name": "4 Jul",
+      "value": 78
+    },
+    {
+      "name": "5 Jul",
+      "value": 12
+    },
+    {
+      "name": "6 Jul",
+      "value": 120
+    },
+    {
+      "name": "7 Jul",
+      "value": 93
+    },
+  ];
+
+  view: any = [];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = false;
+  shodGridLines = false;
+  showXAxisLabel = true;
+  xAxisLabel = 'Days';
+  showYAxisLabel = true;
+  yAxisLabel = 'Minutes';
+  showGridLines = false;
+  barPadding = 20;
+
+  ngOnInit() {
+    this.chartParentWidth = (this.chartParentWidth-100) / 2;
+    this.view = [this.chartParentWidth, this.chartParentHeight+100];
+    // console.log(this.chartParentHeight);
+  }
+
+  // ngAfterContentInit() {
+  //   console.log(this.chart);
+  // }
+
+  // ngAfterContentChecked() {
+  //   this.cdref.detectChanges();
+  // }
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,25 +1,38 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef } from '@angular/core';
 import { User } from '../Interface/user';
 import { Store, select } from '@ngrx/store';
 import { Observable, map } from 'rxjs';
-import { name } from '../store/user/user.selector';
+import { name } from '../Store/user/user.selector';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
+
 export class DashboardComponent {
   user$: Observable<string>;
   user: string;
-  //helloAlienIcon: string = "../icons/green-alien-hello.svg";
+  chartParentWidth:number;
+  chartParentHeight:number;
 
-  constructor(private store: Store<{userStore: User}>){
+
+  constructor(private store: Store<{userStore: User}>, private chart: ElementRef, private cdref: ChangeDetectorRef){
     this.user$ = this.store.pipe(
       select('userStore'),
       map(state => name(state))
     )
   };
+
+  ngAfterContentInit() {
+    this.chartParentWidth = this.chart.nativeElement.offsetWidth;
+    this.chartParentHeight = this.chart.nativeElement.offsetHeight;
+
+  }
+
+  ngAfterContentChecked() {
+    this.cdref.detectChanges();
+  }
 
   ngOnInit() {
     this.user$.subscribe((user) => {
